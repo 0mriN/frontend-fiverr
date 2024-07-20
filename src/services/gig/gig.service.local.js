03a0d4f997c20638c7a1638b8f41880dee52439b
Idan Dujovne <idan.duj@gmail.com>
2024-07-20 15:29:00 +0300

    Update basic data

--- src/services/gig/gig.service.local.js
+++ src/services/gig/gig.service.local.js
@@ -1,287 +1,12 @@
 
 import { storageService } from '../async-storage.service'
-import { makeId, saveToStorage } from '../util.service'
+import { getRandomIntInclusive, makeId, makeLorem, saveToStorage, loadFromStorage } from '../util.service'
 import { userService } from '../user'
-import {saveToStorage} from '../util.service.js'
 
 const STORAGE_KEY = 'gig'
+var gigs = loadFromStorage(STORAGE_KEY)
+_createGigs()
 
-const gigs = [{
-    _id: 'g101',
-    title: 'I will design your logo in just 24 hrs in modern style',
-    aboutDesc:`<p>
-  Our <mark>Logo Maker</mark> is the best tool for your branding needs. With our platform, you can:
-  <ul>
-    <li><strong>Create</strong> stunning logos effortlessly</li>
-    <li><strong>Customize</strong> every aspect to fit your brand</li>
-    <li><strong>Download</strong> high-resolution files instantly</li>
-  </ul>
-  Join thousands of satisfied users and elevate your brand today!
-</p>
-`,
-aboutFiles:`    <div class="logo-details">
-        <div class="detail-item">
-            <span class="detail-title">Logo style</span>
-            <span class="detail-content">Minimalist</span>
-        </div>
-        <div class="detail-item">
-            <span class="detail-title">File format</span>
-            <span class="detail-content">AI, JPG, PNG, SVG</span>
-        </div>
-    </div>`,
-    price: 10.00,
-    owner: {
-        _id: 'u101',
-        fullname: 'Shushu Sha',
-        imgUrl: './src/assets/img/profile.png',
-        level: 'Top Rated',
-        rate: 4,
-    },
-    daysToMake: 3,
-    description: 'Make unique logo...',
-    avgResponseTime: 1,
-    loc: 'Ghana',
-    imgUrls: ['./src/assets/img/img1.png'],
-    tags: ['Arts And Crafts', 'Logo Design'],
-    likedByUsers: ['mini-user'],
-    reviews: [
-        {
-            id: 'madeId',
-            txt: 'Did an amazing work',
-            rate: 4,
-            by: {
-                _id: 'u102',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },{
-            id: 'madeId',
-            txt: 'Did an awesome work',
-            rate: 5,
-            by: {
-                _id: 'u107',
-                fullname: 'user7',
-                imgUrl: '/img/img2.jpg',
-            },
-        },{
-            id: 'madeId',
-            txt: 'Did a great job',
-            rate: 2,
-            by: {
-                _id: 'u102',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },
-    ],
-}, {
-    _id: 'g102',
-    title: 'I will design a timeless unique 3d buisness logo design',
-    aboutDesc:`<p>
-  Discover the ultimate <mark>Logo Design Tool</mark> for your business. You can:
-  <ul>
-    <li><strong>Generate</strong> unique logos in minutes</li>
-    <li><strong>Edit</strong> with an intuitive interface</li>
-    <li><strong>Save</strong> multiple versions for different uses</li>
-  </ul>
-  Start now and see the difference a professional logo can make!
-</p>`,
-aboutFiles:`    <div class="logo-details">
-        <div class="detail-item">
-            <span class="detail-title">Logo style</span>
-            <span class="detail-content">Minimalist</span>
-        </div>
-        <div class="detail-item">
-            <span class="detail-title">File format</span>
-            <span class="detail-content">AI, JPG, PNG, SVG</span>
-        </div>
-    </div>`,
-    price: 12.16,
-    owner: {
-        _id: 'u102',
-        fullname: 'Dudu Da',
-        imgUrl: './src/assets/img/profile.png',
-        level: 'Level 1',
-        rate: 4,
-    },
-    daysToMake: 3,
-    description: 'Make unique logo...',
-    avgResponseTime: 1,
-    loc: 'Ghana',
-    imgUrls: ['./src/assets/img/img2.png'],
-    tags: ['Arts And Crafts', 'Logo Design'],
-    likedByUsers: ['mini-user'],
-    reviews: [
-        {
-            id: 'madeId',
-            txt: 'Did an amazing work',
-            rate: 4,
-            by: {
-                _id: 'u103',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },
-    ],
-}, {
-    _id: 'g105',
-    title: 'I will create a hand drawn logo',
-    aboutDesc:`<p>
-  Enhance your brand with our powerful <mark>Logo Creator</mark>. Features include:
-  <ul>
-    <li><strong>Design</strong> with a wide variety of templates</li>
-    <li><strong>Personalize</strong> with colors, fonts, and icons</li>
-    <li><strong>Export</strong> in multiple formats for web and print</li>
-  </ul>
-  Get started today and give your business the logo it deserves!
-</p>`,
-aboutFiles:`    <div class="logo-details">
-        <div class="detail-item">
-            <span class="detail-title">Logo style</span>
-            <span class="detail-content">Minimalist</span>
-        </div>
-        <div class="detail-item">
-            <span class="detail-title">File format</span>
-            <span class="detail-content">AI, JPG, PNG, SVG</span>
-        </div>
-    </div>`,
-    price: 35.90,
-    owner: {
-        _id: 'u103',
-        fullname: 'Bubu Ba',
-        imgUrl: './src/assets/img/profile.png',
-        level: 'Level 2',
-        rate: 4,
-    },
-    daysToMake: 3,
-    description: 'Make unique logo...',
-    avgResponseTime: 1,
-    loc: 'Ghana',
-    imgUrls: ['./src/assets/img/img3.png'],
-    tags: ['Arts And Crafts', 'Logo Design'],
-    likedByUsers: ['mini-user'],
-    reviews: [
-        {
-            id: 'madeId',
-            txt: 'Did an amazing work',
-            rate: 4,
-            by: {
-                _id: 'u104',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },
-    ],
-},{
-    _id: 'g106',
-    title: 'I will design your logo in just 24 hrs in modern style',
-    price: 10.00,
-    owner: {
-        _id: 'u101',
-        fullname: 'Shushu Sha',
-        imgUrl: './src/assets/img/profile.png',
-        level: 'Top Rated',
-        rate: 4,
-    },
-    daysToMake: 3,
-    description: 'Make unique logo...',
-    avgResponseTime: 1,
-    loc: 'Ghana',
-    imgUrls: ['./src/assets/img/img1.png'],
-    tags: ['Arts And Crafts', 'Logo Design'],
-    likedByUsers: ['mini-user'],
-    reviews: [
-        {
-            id: 'madeId',
-            txt: 'Did an amazing work',
-            rate: 4,
-            by: {
-                _id: 'u102',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },{
-            id: 'madeId',
-            txt: 'Did an awesome work',
-            rate: 5,
-            by: {
-                _id: 'u107',
-                fullname: 'user7',
-                imgUrl: '/img/img2.jpg',
-            },
-        },{
-            id: 'madeId',
-            txt: 'Did a great job',
-            rate: 2,
-            by: {
-                _id: 'u102',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },
-    ],
-}, {
-    _id: 'g107',
-    title: 'I will design a timeless unique 3d buisness logo design',
-    price: 12.16,
-    owner: {
-        _id: 'u102',
-        fullname: 'Dudu Da',
-        imgUrl: './src/assets/img/profile.png',
-        level: 'Level 1',
-        rate: 4,
-    },
-    daysToMake: 3,
-    description: 'Make unique logo...',
-    avgResponseTime: 1,
-    loc: 'Ghana',
-    imgUrls: ['./src/assets/img/img2.png'],
-    tags: ['Arts And Crafts', 'Logo Design'],
-    likedByUsers: ['mini-user'],
-    reviews: [
-        {
-            id: 'madeId',
-            txt: 'Did an amazing work',
-            rate: 4,
-            by: {
-                _id: 'u103',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },
-    ],
-}, {
-    _id: 'g108',
-    title: 'I will create a hand drawn logo',
-    price: 35.90,
-    owner: {
-        _id: 'u103',
-        fullname: 'Bubu Ba',
-        imgUrl: './src/assets/img/profile.png',
-        level: 'Level 2',
-        rate: 4,
-    },
-    daysToMake: 3,
-    description: 'Make unique logo...',
-    avgResponseTime: 1,
-    loc: 'Ghana',
-    imgUrls: ['./src/assets/img/img3.png'],
-    tags: ['Arts And Crafts', 'Logo Design'],
-    likedByUsers: ['mini-user'],
-    reviews: [
-        {
-            id: 'madeId',
-            txt: 'Did an amazing work',
-            rate: 4,
-            by: {
-                _id: 'u104',
-                fullname: 'user2',
-                imgUrl: '/img/img2.jpg',
-            },
-        },
-    ],
-},
-]
 export const gigService = {
     query,
     getById,
@@ -293,27 +18,27 @@ window.cs = gigService
 
 
 async function query(filterBy = { txt: '', price: 0 }) {
-    if (!gigs) saveToStorage(STORAGE_KEY, _getDemoGigs())
-    var gigs = await storageService.query(STORAGE_KEY)
-    const { txt, minSpeed, maxPrice, sortField, sortDir } = filterBy
+    // gigs = await storageService.query(STORAGE_KEY)
+    // const { txt, minSpeed, maxPrice, sortField, sortDir } = filterBy
 
-    if (txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        gigs = gigs.filter(gig => regex.test(gig.vendor) || regex.test(gig.description))
-    }
-    if (minSpeed) {
-        gigs = gigs.filter(gig => gig.speed >= minSpeed)
-    }
-    if (sortField === 'vendor' || sortField === 'owner') {
-        gigs.sort((gig1, gig2) =>
-            gig1[sortField].localeCompare(gig2[sortField]) * +sortDir)
-    }
-    if (sortField === 'price' || sortField === 'speed') {
-        gigs.sort((gig1, gig2) =>
-            (gig1[sortField] - gig2[sortField]) * +sortDir)
-    }
+    // if (txt) {
+    //     const regex = new RegExp(filterBy.txt, 'i')
+    //     gigs = gigs.filter(gig => regex.test(gig.vendor) || regex.test(gig.description))
+    // }
+    // if (minSpeed) {
+    //     gigs = gigs.filter(gig => gig.speed >= minSpeed)
+    // }
+    // if (sortField === 'vendor' || sortField === 'owner') {
+    //     gigs.sort((gig1, gig2) =>
+    //         gig1[sortField].localeCompare(gig2[sortField]) * +sortDir)
+    // }
+    // if (sortField === 'price' || sortField === 'speed') {
+    //     gigs.sort((gig1, gig2) =>
+    //         (gig1[sortField] - gig2[sortField]) * +sortDir)
+    // }
 
-    gigs = gigs.map(({ _id, vendor, price, speed, owner }) => ({ _id, vendor, price, speed, owner }))
+    // gigs = gigs.map(({ _id, vendor, price, speed, owner }) => ({ _id, vendor, price, speed, owner }))
+    console.log('bla',gigs);
     return gigs
 }
 
@@ -362,4 +87,102 @@ async function addGigMsg(gigId, txt) {
     await storageService.put(STORAGE_KEY, gig)
 
     return msg
+}
+
+function _createGigs() {
+    
+    if (!gigs || !gigs.length) {
+        gigs = [
+            _createGig(),
+            _createGig('this is a gig'),
+            _createGig('ill design your logo'),
+            _createGig('i make gigs'),
+            _createGig('i have a good gig'),
+            _createGig('i can do a good gig'),
+            _createGig('hire me to do a gig'),
+            _createGig('i make gigs for youtube'),
+        ]
+        saveToStorage(STORAGE_KEY, gigs)
+    }
+}
+
+function _createGig(title = 'a new gig') {
+    return {
+        id: makeId(),
+        title,
+        aboutDesc: `
+            <p>
+                Our <mark>Logo Maker</mark> is the best tool for your branding needs. With our platform, you can:
+                <ul>
+                    <li><strong>Create</strong> stunning logos effortlessly</li>
+                    <li><strong>Customize</strong> every aspect to fit your brand</li>
+                    <li><strong>Download</strong> high-resolution files instantly</li>
+                </ul>
+                Join thousands of satisfied users and elevate your brand today!
+            </p>`,
+        aboutFiles: `
+            <div class="logo-details">
+                <div class="detail-item">
+                    <span class="detail-title">Logo style</span>
+                    <span class="detail-content">Minimalist</span>
+                </div>
+                <div class="detail-item">
+                    <span class="detail-title">File format</span>
+                    <span class="detail-content">AI, JPG, PNG, SVG</span>
+                </div>
+            </div>`,
+        price: getRandomIntInclusive(5, 50),
+        owner: {
+            _id: makeId(),
+            fullname: makeLorem(2),
+            imgUrl: '../src/assets/img/profile.png',
+            level: _getOwnerLevel(),
+            rate: getRandomIntInclusive(1, 5),
+        },
+        daysToMake: getRandomIntInclusive(1, 7),
+        description: 'Make unique logo...',
+        avgResponseTime: getRandomIntInclusive(1, 5),
+        imgUrls: [
+            '../src/assets/img/img1.png',
+            '../src/assets/img/img2.png',
+            '../src/assets/img/img3.png',
+        ],
+        tags: ['Arts And Crafts', 'Logo Design'],
+        likedByUsers: ['mini-user'],
+        reviews: [
+            {
+                id: 'madeId',
+                txt: 'Did an amazing work',
+                rate: getRandomIntInclusive(1, 5),
+                by: {
+                    _id: 'u102',
+                    fullname: 'user2',
+                    imgUrl: '/img/img2.jpg',
+                },
+            }, {
+                id: 'madeId',
+                txt: 'Did an awesome work',
+                rate: getRandomIntInclusive(1, 5),
+                by: {
+                    _id: 'u107',
+                    fullname: 'user7',
+                    imgUrl: '/img/img2.jpg',
+                },
+            }, {
+                id: 'madeId',
+                txt: 'Did a great job',
+                rate: getRandomIntInclusive(1, 5),
+                by: {
+                    _id: 'u102',
+                    fullname: 'user2',
+                    imgUrl: '/img/img2.jpg',
+                },
+            },
+        ],
+    }
+}
+
+function _getOwnerLevel() {
+    const levels = ['Level 1', 'Level 2', 'Top Rated']
+    return levels[getRandomIntInclusive(0, 2)]
 }
