be159dc8102c058d665bd34507be1169c2ad95fb
Idan Dujovne <idan.duj@gmail.com>
2024-07-21 11:39:19 +0300

    Make header responsive

--- src/pages/GigIndex.jsx
+++ src/pages/GigIndex.jsx
@@ -56,10 +56,10 @@ export function GigIndex() {
     return (
         <main className="gig-index">
             <header>
-                <h2>Gigs</h2>
+                {/* <h2>Gigs</h2> */}
                 {userService.getLoggedinUser() && <button onClick={onAddGig}>Add a Gig</button>}
             </header>
-            <GigFilter filterBy={filterBy} setFilterBy={setFilterBy} />
+            {/* <GigFilter filterBy={filterBy} setFilterBy={setFilterBy} /> */}
             <GigList
                 gigs={gigs}
                 onRemoveGig={onRemoveGig}
