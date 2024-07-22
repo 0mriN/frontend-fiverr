a11ac4d122befc56ac030adac1cb6f7e8a6813e2
Omri Nadav <omrinadav15@gmail.com>
2024-07-22 17:26:11 +0300

    Fix svg and add some coms to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,23 @@
 import React from 'react'
+import meta from '../assets/img/svg/meta.svg'
+import google from '../assets/img/svg/google.svg'
+import netflix from '../assets/img/svg/netflix.svg'
+import payoneer from '../assets/img/svg/payoneer.svg'
+import paypal from '../assets/img/svg/paypal.svg'
+import pg from '../assets/img/svg/pg.svg'
+import programming from '../assets/img/svg/programming-tech.svg'
+import heroImg from '../assets/img/home-page-hero.png'
+import searchBtn from '../assets/img/svg/SearchBtn.svg'
+import graphicsDesign from '../assets/img/svg/graphics-design.svg'
+import digitalMarketing from '../assets/img/svg/digital-marketing.svg'
+import writingTranslation from '../assets/img/svg/writing-translation.svg'
+import videoAnimation from '../assets/img/svg/video-animation.svg'
+import aiServices from '../assets/img/svg/ai-services.svg'
+import musicAudio from '../assets/img/svg/music-audio.svg'
+import business from '../assets/img/svg/business.svg'
+import consulting from '../assets/img/svg/consulting.svg'
+
+
 
 export function HomePage() {
     return (
@@ -8,30 +27,62 @@ export function HomePage() {
                     <h1>Find the right <span>freelance</span> service, right away</h1>
                     <div className="search-bar">
                         <input type="text" placeholder="Search for any service..." />
-                        <button className="btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" id="search"><g fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"><g stroke="white" strokeWidth="2" transform="translate(-1687 -1941)"><g transform="translate(1688 1942)"><circle cx="7.5" cy="7.5" r="7.5"></circle><path d="M18 18l-5.2-5.2"></path></g></g></g></svg>
+                        <button>
+                            <img src={searchBtn} alt="searchbtn" className='search-btn' />
                         </button>
                     </div>
                 </div>
                 <div className="hero-images">
-                <img src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/678686c47c024e18e773c75e90aaab1c-1705999902381/hero-xl-x1.png" alt="Jenny" className="hero-image" />
+                    <img src={heroImg} alt="hero" className="hero-image" />
                 </div>
                 <div className="trusted-by">
                     <span>Trusted by:</span>
                     <ul>
-                        <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/meta.398bc1c.svg" alt="meta" /></li>
-                        <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/google.129f8ec.svg" alt="Google"/></li>
-                        <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/netflix.e1b0070.svg" alt="NETFLIX"/></li>
-                        <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/pg.a47f1ab.svg" alt="P&G"/></li>
-                        <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/paypal.0520267.svg" alt="PayPal"/></li>
-                        <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/payoneer.67915a0.svg" alt="Payoneer"/></li>
+                        <li><img src={meta} alt="meta" /></li>
+                        <li><img src={google} alt="Google" /></li>
+                        <li><img src={netflix} alt="NETFLIX" /></li>
+                        <li><img src={pg} alt="P&G" /></li>
+                        <li><img src={paypal} alt="PayPal" /></li>
+                        <li><img src={payoneer} alt="Payoneer" /></li>
                     </ul>
                 </div>
             </div>
             <div className='sub-category-container'>
                 <a href="">
-                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/programming-tech.49dbf0d.svg" alt="programming_tech" loading="lazy"/>
-                <p class="sub-category">Programming & Tech</p>
+                    <img src={programming} alt="programming_tech" loading="lazy" />
+                    <p className="sub-category">Programming & Tech</p>
+                </a>
+                <a href="">
+                    <img src={graphicsDesign} alt="graphics_design" loading="lazy" />
+                    <p className="sub-category">Graphics & Design</p>
+                </a>
+                <a href="">
+                    <img src={digitalMarketing} alt="digital_marketing" loading="lazy" />
+                    <p className="sub-category">Digital Marketing </p>
+                </a>
+                <a href="">
+                    <img src={writingTranslation} alt="writing_translation" loading="lazy" />
+                    <p className="sub-category">Writing & Translation</p>
+                </a>
+                <a href="">
+                    <img src={videoAnimation} alt="video_animation" loading="lazy" />
+                    <p className="sub-category">Video & Animation</p>
+                </a>
+                <a href="">
+                    <img src={aiServices} alt="ai_services" loading="lazy" />
+                    <p className="sub-category">AI Services</p>
+                </a>
+                <a href="">
+                    <img src={musicAudio} alt="Music_audio" loading="lazy" />
+                    <p className="sub-category">Music & Audio</p>
+                </a>
+                <a href="">
+                    <img src={business} alt="business" loading="lazy" />
+                    <p className="sub-category">Business</p>
+                </a>
+                <a href="">
+                    <img src={consulting} alt="consulting" loading="lazy" />
+                    <p className="sub-category">Consulting</p>
                 </a>
             </div>
             {/* <div className='video-container'> */}
