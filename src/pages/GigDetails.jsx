45239f07e6ba6492486a8082afd390cc50ba1adc
Omri Nadav <omrinadav15@gmail.com>
2024-07-24 23:15:12 +0300

    Bug fix and clean up

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { addGigMsg, loadGig } from "../store/actions/gig.actions"
 import { PackModal } from "../cmps/PackModal"
 import { DetailsMainCarousel } from "../cmps/DetailsMainCarousel"
+
 import { DetailsHeader } from "../cmps/DetailsHeader"
 import { DetailsReviews } from "../cmps/DetailsReviews"
 import { DetailsReview } from "../cmps/DetailsReview"
@@ -21,18 +22,18 @@ export function GigDetails() {
 
 
   return (
+
     <section className="gig-details">
       <div className="gig-details-main">
         {gig ? (
           <div>
             <DetailsHeader gig={gig} />
             <section className="main-carousel">
-              <HeroCarousel gig={gig} />
+              {/* <DetailsMainCarousel gig={gig} /> */}
             </section>
             <div>
-              <DetailsHeader gig={gig} />
-              {/* <HeroCarousel images={images} /> */}
-              <DetailsMainCarousel imgUrls={gig.imgUrls} />
+              <DetailsAbout gig={gig} />
+              {/* <DetailsReview gig={gig}/> */}
             </div>
             {/* <PackageComparison/> */}
             <DetailsReviews gig={gig} />
@@ -49,46 +50,3 @@ export function GigDetails() {
     </section>
   )
 }
-
-
-
-// export function GigDetails() {
-//   const { gigId } = useParams()
-//   const gig = useSelector((storeState) => storeState.gigModule.gig)
-//   const dispatch = useDispatch()
-
-//   useEffect(() => {
-//     const fetchGig = async () => {
-//       try {
-//         await dispatch(loadGig(gigId))
-//       } catch (error) {
-//         console.error('Failed to load gig:', error)
-//       }
-//     }
-//     fetchGig()
-//   }, [gigId, dispatch])
-
-//   if (!gig) {
-//     return <p>Loading...</p>
-//   }
-
-//   const imgs = gig.imgUrls || []
-
-//   return (
-//     <section className="gig-details">
-//       <div className="gig-details-main">
-//         <DetailsHeader gig={gig} />
-//         <HeroCarousel images={imgs} />
-//         {/* <DetailsReview gig={gig}/> */}
-//         <DetailsAbout gig={gig} />
-//         {/* <PackageComparison/> */}
-//         <DetailsReviews gig={gig} />
-//       </div>
-
-//       <div className="gig-buy-modal">
-//         {/* {gig && */}
-//         {/* // <PackModal gig={gig} />} */}
-//       </div>
-//     </section>
-//   )
-// }
