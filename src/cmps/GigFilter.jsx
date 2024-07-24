235c7caa3d0ab699f56116a96108a283f8f9b060
Idan Dujovne <idan.duj@gmail.com>
2024-07-24 14:32:29 +0300

    Style GigIndex

--- src/cmps/GigFilter.jsx
+++ src/cmps/GigFilter.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux"
 import homeBreadcrumb from '../assets/img/svg/home-breadcrumb.svg'
 import { Link } from "react-router-dom"
+import downArrow from '../assets/img/svg/down-arrow.svg'
 
 export function GigFilter({ gigs }) {
     const filterBy = useSelector(storeState => storeState.gigModule.filterBy)
 
     function handleChange({ target }) {
         let { value, name: field, type, checked } = target
-        
+
         switch (type) {
             case 'number':
             case 'range':
@@ -22,30 +23,39 @@ export function GigFilter({ gigs }) {
     }
 
     return <section className="gig-filter">
-    
-        <article className='title-wrapper'>
-            <ul className='breadcrumbs'>
-                <li><Link to="/"> <img src={homeBreadcrumb} alt="Fiverr"></img></Link></li>
-                <li> / {filterBy.tags[0]} </li>
+
+        <article className="title-container">
+            <ul className="breadcrumbs flex align-center">
+                <li className="flex align-center"><Link to="/"><img src={homeBreadcrumb} alt="Fiverr"></img></Link>&nbsp;</li>
+                <li> / &nbsp;{filterBy.tags[0]} </li>
             </ul>
             <h1>Website Development</h1>
-            <div><p className="index-subtitle">Create, build, and develop your website with skilled website developers</p></div>
+            <p className="index-subtitle">Create, build, and develop your website with skilled website developers</p>
             {/* {userService.getLoggedinUser() && <button onClick={onAddGig}>Add a Gig</button>} */}
         </article>
-        <article className='sort-container'>
-            <div className='top-filters'>
-                <select onChange={handleChange}>budget
-                    <option value="value">Value</option>
-                    <option value="midRange">Mid-range</option>
-                    <option value="highEnd">High-end</option>
-                </select>
-                <button>Delivery time</button>
+
+        <article className="filter-container">
+            <div className="btn-container">
+                <button className="filter-btn budget flex align-center justify-center">
+                    Budget
+                    <svg width="16" height="16" viewBox="0 0 11 7" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M5.464 6.389.839 1.769a.38.38 0 0 1 0-.535l.619-.623a.373.373 0 0 1 .531 0l3.74 3.73L9.47.61a.373.373 0 0 1 .531 0l.619.623a.38.38 0 0 1 0 .535l-4.624 4.62a.373.373 0 0 1-.531 0Z" /></svg>
+                </button>
             </div>
-            <ul className='sort-by-wrapper'>
+
+            <div className="btn-container">
+                <button className="filter-btn delivery-time flex align-center justify-center">
+                    Delivery time
+                    <svg width="16" height="16" viewBox="0 0 11 7" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M5.464 6.389.839 1.769a.38.38 0 0 1 0-.535l.619-.623a.373.373 0 0 1 .531 0l3.74 3.73L9.47.61a.373.373 0 0 1 .531 0l.619.623a.38.38 0 0 1 0 .535l-4.624 4.62a.373.373 0 0 1-.531 0Z" /></svg>
+                </button>
+            </div>
+        </article>
+
+        <article className="sort-container">
+            <ul className="sort-by-wrapper flex">
                 <li>{gigs.length}+ Results</li>
-                <li className='sort-by'>
+                <li className="sort-by">
                     <span>Sort By:</span>
-                    <select className='sort-selector'>Best selling
+                    <select className="sort-select">Best selling
                         <option value="">Best selling</option>
                         <option value="">Recommended</option>
                         <option value="">Newest arrivals</option>
@@ -53,5 +63,6 @@ export function GigFilter({ gigs }) {
                 </li>
             </ul>
         </article>
+
     </section>
 }
